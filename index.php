c3eec35a59b8a960544d7df7c2268e92c6e78f9a
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2023-01-07 15:47:20 +0100

    bo

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!--meta -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+
+    <!-- css/style.css --->
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <title>Document</title>
+
+
+</head>
+
+<body>
+    <div class="debug p-4 m-5">
+
+        <h1 class="p-3">
+            <?='hello world' ?>
+        </h1>
+
+        <div class="d-flex gap-3 ">
+
+            <div class="w-30">
+                <img src="https://api.lorem.space/image/game?w=300&h=440" alt="">
+            </div>
+
+            <div class="w-25">
+
+                <h3 class="py-4">Titolo Gioco</h3>
+
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga temporibus eligendi iure. Pariatur
+                    fugiat sequi facilis mollitia voluptates minima, vel veniam dolorem reiciendis vero molestiae ipsam
+                    praesentium recusandae repudiandae harum?
+
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque, est amet aliquid doloremque
+                </p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <h1 class="px-5 py-2">Titolo Grande</h1>
+
+    <div class="debug d-flex p-4 m-5">
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, consequatur? Assumenda necessitatibus eius
+            laborum corrupti iste soluta incidunt officia illo impedit quam cum architecto fugiat corporis,
+            perspiciatis,
+            nobis voluptatibus sed.
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem vero non repellat officiis distinctio, fugiat,
+            nisi
+            quae temporibus impedit saepe delectus est debitis. Repellat voluptas, tenetur velit officia veritatis ab!
+        </p>
+
+    </div>
+
+</body>
+
+</html>
